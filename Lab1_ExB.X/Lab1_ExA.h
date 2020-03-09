e893da2fa1ba8aacd5428a8a9d800750fe952963
TheJade <TheJade@users.noreply.github.com>
2020-03-09 14:42:04 -0600

    Exercise C ish

---
+++ Lab1_ExB.X/Lab1_ExA.h
@@ -0,0 +1,12 @@
+// Lab1_ExA_Library.X.a contains pre-compiled functions for the PIC16F1778. 
+// This file provides function prototypes so they can be used in main.
+
+#ifndef LAB1_EXA_H
+#define	LAB1_EXA_H
+
+void Lab1_ConfigurePins(void);
+void Lab1_LEDPattern_Andy(void);
+void Lab1_SwitchToLED(void);
+
+#endif	/* LAB1_EXA_H */
+
---
+++ Lab1_ExB.X/build/default/debug/main.p1.d
@@ -0,0 +1,3 @@
+build/default/debug/main.p1:  \
+main.c  \
+Lab1_ExA.h 
