8b050ccf6018c2c7879a549654020a7a83f2d29b
JustinRoss911 <JustinRoss911@users.noreply.github.com>
2020-03-11 15:22:53 -0600

    RC6 Button is now an on/off button

--- Lab1_ExB.X/RC6_Button.c
+++ Lab1_ExB.X/RC6_Button.c
@@ -16,13 +16,18 @@ void main(void) {
     
     /*Pin Configuration*/
     TRISB = 0;
-    TRISCbits.TRISC7 = 1;
-    TRISCbits.TRISC6 = 0;
+    TRISCbits.TRISC7 = 0;
+    TRISCbits.TRISC6 = 1;
     ANSELB = 0;
     ANSELC = 0;
     PORTB = 0x0;
     PORTCbits.RC7 = 0;
     PORTCbits.RC6 = 0;
+    
+    /*PIC User Manual Chapter 13: Interrupts
+     This is our configuration to set up RC6 as a interrupt on rising edge logic*/
+    INTCONbits.IOCIE = 1; //Tells Processor to allow for interrupts to occur.
+    IOCCPbits.IOCCP6 = 1; //Assigns RC6 to interrupt on rising edge.
     
     while(1)
     {
@@ -32,17 +37,23 @@ void main(void) {
 }
 void RC6button(unsigned char a)
 {
-    /*Need to refine code so that the switch is uniquely set to a bit, right now it resets all bits in PORTB to 0
-     we want to make it so that it only sets bits (a) to zero*/
+    /*This function is designed to set bits a to an on/off logic switch*/
+    
+    /*Need to refine code so that the switch is uniquely set to bits (a), right now it resets all bits in PORTB to 0
+     we want to make it so that it only sets bits a to zero*/
     static int swi = 1;
-    if(PORTCbits.RC6 == 1 && swi == 1)
+
+    if(IOCCFbits.IOCCF6 == 1 && swi == 1) // If flag is true and switch is set to off
     {
+        IOCCFbits.IOCCF6 = 0; // Removes flag
         PORTB = a | PORTB;
         swi = 0;
     }
-    else if(PORTCbits.RC6 == 1 && swi == 0)
+    else if(IOCCFbits.IOCCF6 == 1 && swi == 0) // If flag is true and switch is set to on
     {
+        IOCCFbits.IOCCF6 = 0; // Removes flag
         PORTB = 0;
         swi = 1;
     }
 }
+
